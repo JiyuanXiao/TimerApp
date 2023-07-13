b911b5eb48d3c157a0b4645e9342edb2ba159b75
Ian <ian@g.ucla.edu>
2023-07-12 23:09:39 -0700

    test push

--- src/features/Timing.js
+++ src/features/Timing.js
@@ -4,15 +4,15 @@ import { RoundedButton } from '../components/RoundedButton';
 export const Timing = ({ onChangeTime }) => {
   return (
     <>
-    <View style={styles.timingButton}>
-      <RoundedButton size={75} title="15" onPress={() => onChangeTime(15)} />
-    </View>
-    <View style={styles.timingButton}>
-      <RoundedButton size={75} title="20" onPress={() => onChangeTime(20)} />
-    </View>
-    <View style={styles.timingButton}>
-      <RoundedButton size={75} title="40" onPress={() => onChangeTime(40)} />
-    </View>
+      <View style={styles.timingButton}>
+        <RoundedButton size={75} title="15" onPress={() => onChangeTime(15)} />
+      </View>
+      <View style={styles.timingButton}>
+        <RoundedButton size={75} title="20" onPress={() => onChangeTime(20)} />
+      </View>
+      <View style={styles.timingButton}>
+        <RoundedButton size={75} title="40" onPress={() => onChangeTime(40)} />
+      </View>
     </>
   );
 };
